e3ed3f81257b2f811f493a0b668cd63bf6ba41a5
Burhaann <burhaann321@gmail.com>
2024-02-19 17:38:24 +0530

    Show Random Numbers by clicking the ROLL DIE button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import React from "react";
+import Die from "./Die";
 function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => <Die value={die} />);
+
   return (
     <>
-      <main></main>
+      <div className="container">
+        <main>
+          <div className="dice-container">{diceElements}</div>
+          <button className="roll-dice" onClick={rollDice}>
+            Roll
+          </button>
+        </main>
+      </div>
     </>
   );
 }
